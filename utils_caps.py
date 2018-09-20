f2fc1c0f21716d122c2d49acb49d3f38e99d1413
Marcelo Gutierrez <m.gutierrez@braintree.com>
2018-09-20 15:55:22 +0100

    commit_line

--- utils_caps.py
+++ utils_caps.py
@@ -7,12 +7,14 @@ def unpickle(file):
         dictio = pickle.load(fo, encoding='bytes')
     return dictio
 
-def load_image_data(data_path):
+def load_image_data(data_path, normalize=True ):
     if 'cifar' in data_path:
         dictio_data = unpickle(data_path)
         X = dictio_data[b'data'].reshape([10000,3,32,32]).transpose(0,2,3,1)
         y = np.array(dictio_data[b'labels'])
         print('shape : ', X.shape)
+        if normalize == True:
+            X = X/255
         return X,y
 
 
@@ -22,3 +24,21 @@ def subsample(X_train, y_train, ratio=0.1):
     sample_y = y_train[idx]
     sample_x = X_train[idx]
     return sample_x,sample_y
+
+
+def get_number_parameters(tf):
+    total_parameters = 0
+    for variable in tf.trainable_variables():
+        # shape is an array of tf.Dimension
+        shape = variable.get_shape()
+        # print()
+        #print('Name: {}   -   '.format(variable.name) + 'Shape: ', shape)
+        # print(len(shape))
+        variable_parameters = 1
+        for dim in shape:
+            # print(dim)
+            variable_parameters *= dim.value
+        #print('Number of parameters: {}'.format(variable_parameters))
+        total_parameters += variable_parameters
+    print('Total Number of parameters: {}'.format(total_parameters))
+    return total_parameters
